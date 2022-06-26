ad0e10f0a33734cc92ff23d33ba0a69ccd37f55a
Ivan Gaudino <s257971@studenti.polito.it>
2022-06-26 16:57:49 +0200

    Soluzione

--- src/main/java/it/polito/tdp/bar/model/Event.java
+++ src/main/java/it/polito/tdp/bar/model/Event.java
@@ -1,5 +1,62 @@
 package it.polito.tdp.bar.model;
 
-public class Event {
+import java.time.Duration;
+
+public class Event implements Comparable<Event>{
+	
+	public enum EventType {
+		ARRIVO_GRUPPO_CLIENTI,
+		TAVOLO_LIBERATO
+	}
+	
+	private Duration time;
+	private EventType type;
+	private int nPersone;
+	private Duration durata;
+	private double tolleranza;
+	private Tavolo tavolo;
+	
+	public Event(Duration time, EventType type, int nPersone, Duration durata, double tolleranza, Tavolo tavolo) {
+		super();
+		this.time = time;
+		this.type = type;
+		this.nPersone = nPersone;
+		this.durata = durata;
+		this.tolleranza = tolleranza;
+		this.tavolo = tavolo;
+	}
+
+	public Duration getTime() {
+		return time;
+	}
+
+	public EventType getType() {
+		return type;
+	}
+
+	public int getnPersone() {
+		return nPersone;
+	}
+
+	public Duration getDurata() {
+		return durata;
+	}
+
+	public double getTolleranza() {
+		return tolleranza;
+	}
+
+	public Tavolo getTavolo() {
+		return tavolo;
+	}
+
+	public void setTavolo(Tavolo tavolo) {
+		this.tavolo = tavolo;
+	}
+
+	@Override
+	public int compareTo(Event other) {
+		return this.time.compareTo(other.getTime());
+	}
 
 }
